134b03fb4751bfe6731c44312a1c72264b21c0fb
alnsousa1 <alnprivado@gmail.com>
2024-05-13 22:00:59 -0300

    associando rotas a components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import Home from './components/Home'
 import Devs from './components/Devs'
 import Levels from './components/Levels'
-import {BrowserRouter, Routes, Link, Route} from 'react-router-rom'
+import {BrowserRouter, Routes, Link, Route} from 'react-router-dom';
  
 function App() {
   return (
     <div className="App">
       <h1>Teste Gazin</h1>
+      <BrowserRouter>
       <ul>
-        <li>Home</li>
-        <li>Devs</li>
-        <li>Levels</li>
+        <li> <Link to="/">Home </Link></li>
+        <li> <Link to="/devs">Devs</Link></li>
+        <li> <Link to="/levels">Levels</Link></li>
       </ul>
+
+      <Routes>
+        <Route path='/' index element={<Home/>}></Route>
+        <Route path='/devs' element={<Devs/>}></Route>
+        <Route path='/levels' element={<Levels/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
