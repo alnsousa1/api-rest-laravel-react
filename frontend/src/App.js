00ef2d46599f822404125227837e68ef0d2e6248
alnsousa1 <alnprivado@gmail.com>
2024-05-15 13:00:58 -0300

    estilizando o nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import Home from './components/Home'
 import Devs from './components/Devs'
 import Levels from './components/Levels'
 import {BrowserRouter, Routes, Link, Route} from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 function App() {
   return (
     <div className="App">
       <h1>Teste Gazin</h1>
       <BrowserRouter>
-      <ul>
-        <li> <Link to="/">Home </Link></li>
-        <li> <Link to="/devs">Devs</Link></li>
-        <li> <Link to="/levels">Levels</Link></li>
-      </ul>
+      <Nav variant='tabs'>
+        <Nav.Link as={Link} to={"/"}>Página inicial</Nav.Link>
+        <Nav.Link as={Link} to={"/devs"}>Desenvolvedores</Nav.Link>
+        <Nav.Link as={Link} to={"/levels"}>Níveis</Nav.Link>
+      </Nav>
 
       <Routes>
         <Route path='/' index element={<Home/>}></Route>
