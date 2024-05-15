af50fc2f69f02457efd230161d5b7f87ebfce31d
alnsousa1 <alnprivado@gmail.com>
2024-05-15 13:23:37 -0300

    estilizando a tabela de devs

--- frontend/src/components/Devs.js
+++ frontend/src/components/Devs.js
@@ -1,7 +1,47 @@
-function Devs(){
-    return(
-        <div><p>Tela Devs</p></div>
-    )
+import React from "react";
+import { Table } from "react-bootstrap";
+
+class Devs extends React.Component {
+
+    9
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            devs: []
+        }
+    }
+
+    render() {
+        return (
+            <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Nível</th>
+          <th>Opções</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Allan Sousa</td>
+          <td>Júnior 1</td>
+          <td>Editar  Excluir</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Allan Sousa</td>
+          <td>Júnior 1</td>
+          <td>Editar  Excluir</td>
+        </tr>
+      </tbody>
+    </Table>
+        )
+    }
+
+
 }
 
 export default Devs;
