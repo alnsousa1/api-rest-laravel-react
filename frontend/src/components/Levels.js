17bb326229b29cc47a5483284920fc1c42b5b360
alnsousa1 <alnprivado@gmail.com>
2024-06-06 17:00:08 -0300

    adicionando o modal para os levels

--- frontend/src/components/Levels.js
+++ frontend/src/components/Levels.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Table, Form } from "react-bootstrap";
+import { Button, Table, Form, Modal } from "react-bootstrap";
 
 class Levels extends React.Component {
     constructor(props) {
@@ -7,7 +7,8 @@ class Levels extends React.Component {
 
         this.state = {
             name: '',
-            levels: []
+            levels: [],
+            modalOpened: false
         }
     }
 
@@ -35,6 +36,18 @@ class Levels extends React.Component {
             })
     }
 
+    editarLevel = (id) => {
+        fetch("http://127.0.0.1:8000/api/v1/levels/" + id, { method: 'GET' })
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    id: response.data.id,
+                    name: response.data.name,
+                });
+                this.openModal();
+            })
+    }
+
     cadastrarLevel = (level) => {
         fetch("http://127.0.0.1:8000/api/v1/levels",
             {
@@ -47,6 +60,22 @@ class Levels extends React.Component {
                     this.buscarLevels();
                 } else {
                     alert("Não foi possível cadastrar este nível!");
+                }
+            })
+    }
+
+    atualizarLevel = (level) => {
+        fetch("http://127.0.0.1:8000/api/v1/levels/" + level.id,
+            {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(level)
+            })
+            .then(response => {
+                if (response.ok) {
+                    this.buscarLevels();
+                } else {
+                    alert("Não foi possível editar os dados deste nível!");
                 }
             })
     }
@@ -68,7 +97,8 @@ class Levels extends React.Component {
                                 <td>{level.id}</td>
                                 <td>{level.name}</td>
                                 <td>
-                                    Editar  <Button variant="danger" onClick={() => this.deletarLevel(level.id)}>Excluir</Button>
+                                    <Button variant="primary" onClick={() => this.editarLevel(level.id)}>Editar</Button>
+                                    <Button variant="danger" onClick={() => this.deletarLevel(level.id)}>Excluir</Button>
                                 </td>
                             </tr>
                         )
@@ -85,26 +115,71 @@ class Levels extends React.Component {
     }
 
     submit() {
-        const level = {
-            name: this.state.name,
-        }
 
-        this.cadastrarLevel(level);
+        if (this.state.id == 0) {
+            const level = {
+                name: this.state.name,
+            }
+            this.cadastrarLevel(level);
+        } else {
+            const level = {
+                id: this.state.id,
+                name: this.state.name
+            }
+            this.atualizarLevel(level);
+        }
+        this.handleClose();
     }
+
+    reset = () => {
+        this.setState({
+            id: 0,
+            name: ''
+        })
+        this.openModal();
+    }
+
+    handleClose = () => {
+        this.setState({
+            modalOpened: false
+        })
+    }
+
+    openModal = () => {
+        this.setState({
+            modalOpened: true
+        })
+    }
+
 
     render() {
         return (
             <div>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Nome do nível</Form.Label>
-                        <Form.Control type="text" required placeholder="Digite o nome: " value={this.state.name} onChange={this.updateField('name')} />
-                    </Form.Group>
+                <Modal show={this.state.modalOpened} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Cadastro de Desenvolvedores</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Nome do nível</Form.Label>
+                                <Form.Control type="text" required placeholder="Digite o nome: " value={this.state.name} onChange={this.updateField('name')} />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="button" onClick={() => { this.submit() }}>
+                            Cadastrar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
-                    <Button variant="primary" type="button" onClick={() => {this.submit()}}>
-                        Cadastrar
-                    </Button>
-                </Form>
+                <Button variant="primary" type="button" onClick={() => { this.reset() }}>
+                    Novo
+                </Button>
 
                 {this.renderTabela()}
             </div>
