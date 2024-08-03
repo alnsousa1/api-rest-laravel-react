73d4eee814ba3ff9bd7a7891178f54ca28da4736
alnsousa1 <alnprivado@gmail.com>
2024-08-03 16:25:01 -0300

    crianddo components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import Devs from './components/Devs'
-import Levels from './components/Levels'
+import Cursos from './components/Cursos'
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,17 +7,15 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1 className='m-2'>Gerenciador de Devs</h1>
+        <h1 className='m-2'>Gerenciador de Cursos</h1>
       </div>
       <BrowserRouter>
         <Nav variant='tabs' style={{background: '#fafafa'}}>
-          <Nav.Link as={Link} to={"/devs"}>Desenvolvedores</Nav.Link>
-          <Nav.Link as={Link} to={"/levels"}>Níveis</Nav.Link>
+          <Nav.Link as={Link} to={"/cursos"}>Cursos</Nav.Link>
         </Nav>
 
         <Routes>
-          <Route path='/devs' element={<Devs />}></Route>
-          <Route path='/levels' element={<Levels />}></Route>
+          <Route path='/cursos' element={<Cursos />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
